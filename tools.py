098c4a7bbf49960a6cbebcc4c0f7d89724c31409
MahiKalavadia <mahikalavadia0612@gmail.com>
2026-07-30 11:45:19 +0530

    Changed the cancel event code

--- tools.py
+++ tools.py
@@ -3,13 +3,27 @@ from dotenv import load_dotenv
 from services import load_events, save_events
 from logger import logger
 from datetime import datetime, timedelta
+import json
+from langchain_core.messages import HumanMessage
+from langchain_groq import ChatGroq
 
 load_dotenv()
 
+llm = ChatGroq(model="llama-3.3-70b-versatile")
+
 @tool
-def create_event(title: str, date: str, start_time: str, duration:str = "1 hour") -> str:
+def create_event(title: str | None, date: str, start_time: str, duration:str = "1 hour") -> str:
     """
-    Create a new calendar event.
+    Scheduling a new calender event for the given date and start time.
+    
+    Args:
+    - title: The title of the event inside user's query.
+    - date: The date provided by user in format YYYY-MM-DD and if not provided defaults to today.
+    - start_time: The start_time of the event in 24-hour HH:MM format.
+    - duration: The duration for which the event will occur. Defaults to 1 followed by unit hour.
+
+    Returns:
+    - Message indicating that an event has been created successfully or the message indicating that there is already an existing event occurring at that time.
     """
     try:
         logger.info("Create event tool called!")
@@ -56,9 +70,9 @@ def create_event(title: str, date: str, start_time: str, duration:str = "1 hour"
 
 
 @tool
-def get_events(date: str = ""):
+def get_events(date: str | None):
     """
-    Get all events for a particular date.
+    Get all the events
     """
     try:
         logger.info("Get Events tool called!")
@@ -86,7 +100,15 @@ def get_events(date: str = ""):
 @tool
 def check_availability(date: str, time: str = "") -> str:
     """
-    Check the availability of the time slots provided by user.
+    Checks the availability of the slots by comparing the given date and time with the existing events.
+    Do not ask for time range if the query asked by user doesn't quite need time.
+
+    Args:
+    - date: The date provided in format YYYY-MM-DD. Default date is today.
+    - time: The time in  24-hour HH:MM format.
+
+    Returns:
+    - Returns if the time slot is available or the time slots that are currently available or the message indicating the time slot is occupied.
     """
     try:
         logger.info(f"Check availability tool called!")
@@ -121,16 +143,58 @@ def check_availability(date: str, time: str = "") -> str:
 
 
 @tool
-def cancel_event(date: str, time: str = "") -> str:
+def cancel_event(query:str | None) -> str:
     """
-    Cancel an event. Default date is today
+    Cancel an existing calendar event specified by user.
     """
     try:
         logger.info(f"Cancel event tool called!")
         events = load_events()
         logger.info(f"Events: {events}")
+
+        prompt = f"""
+        You are an event matching assitant.
+
+        User's statement:
+        {query}
+
+        Already existing events:
+        {json.dumps(events, indent=2)}
+
+        Instruction:
+        - identify the best matching event.
+        - Return only the date of the retrieved event from the existing events.
+        - No extra information or text needed.
+        - Just the date in format YYYY-MM-DD.
+        """
+
+        prompt2 = f"""
+        You are an event matching assitant.
+
+        User's statement:
+        {query}
+
+        Already existing events:
+        {json.dumps(events, indent=2)}
+
+        Instruction:
+        - identify the best matching event.
+        - Return only the start time of the retrieved event.
+        - No extra information or text needed.
+        - Just the time in HH:MM
+        """
+
+        
+        llm_response = llm.invoke([HumanMessage(content=prompt)])
+        logger.info(f"Response received by llm for date: {llm_response}")
+        llm_response2 = llm.invoke([HumanMessage(content=prompt2)])
+        logger.info(f"Response received by llm for time: {llm_response2}")
+        event_date = llm_response.content.strip()
+        logger.info(f"Date received by llm: {event_date}")
+        event_time = llm_response2.content.strip()
+        logger.info(f"Time received by llm: {event_time}")
         for event in events:
-            if event["date"] == date and event["start_time"] == time:
+            if event["date"] == event_date and event["start_time"] == event_time:
                 logger.info(f"Removing event.. {event}")
                 events.remove(event)
                 logger.info(f"Saving...")
