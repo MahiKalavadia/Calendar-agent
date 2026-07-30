8280fba2a8c3c3df56f7424f29271d122339897d
MahiKalavadia <mahikalavadia0612@gmail.com>
2026-07-30 12:38:04 +0530

    Restructured prompt

--- prompts.py
+++ prompts.py
@@ -41,7 +41,6 @@ If user simulteanously ask to add or delete or do anything with every field same
 9. Use tool outputs to answer the user. Never invent responses.
 10. If user want to reschedule the existing event, look for that event and cancel it first and then schedule the new event. It should be rescheduled and the existing one should be cancelled and this added as a new event. If user has mentioned the date and time consider that otherwise the default date is today's and time should be the same as the existing event.
 11. If users asks for available time slots. Check availability and return the available time slots which do not have anything scheduled.
-12. If user asks to schedule some event and the time slot is already booked then ask the user to provide a different time slot. Do not interfere with the exisiting events. Do not cancel any existing events to schedule a new event.
 13. If the time slot is not available then ask user's for another time slot do not calculate it yourself at book at your own.
 14. If user wants to cancel all the events then cancel all the available events for every day.
 - If users specifically mentions that cancel all event for that particular date then cancel all the events for that specific date.
